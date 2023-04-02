01de4f5a6740d6cf567d87067c4e86e087f9859a
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-04-03 00:00:03 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleTodo();
+    form.reset();
+  });
+
+  function handleTodo(todo) {
+    todo = document.getElementById("new-task-description").value;
+    let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", deleteToDo);
+    btn.textContent = "X"
+    li.textContent = `${todo} `;
+    let tasks = document.getElementById("tasks");
+    tasks.appendChild(li);
+    li.appendChild(btn);
+  }
+
+  function deleteToDo(e){
+e.target.parentNode.remove();
+  }
 });
